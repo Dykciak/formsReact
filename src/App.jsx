7ee822b43f9bcc984e76992a62212ba390fab304
dykciak <dawid.dykto@gmail.com>
2025-02-09 20:30:48 +0100

    useRef method

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import FormInput from './components/FormInput'
 import './App.css'
 
 function App() {
-  const [username, setUsername] = useState("")
+  // const [username, setUsername] = useState("")
+  const usernameRef = useRef()
 
-  console.log(username);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    console.log(Object.fromEntries(data.entries()));
+  }
   return (
     <>
-      <form>
-         <FormInput placeholder="user420437889" setUsername={setUsername}></FormInput>
-        <FormInput placeholder="your name"></FormInput> 
-        <FormInput placeholder="example@mail.com"></FormInput>
-        <FormInput placeholder="+00 000-000-000"></FormInput> 
+      <form onSubmit={handleSubmit}>
+         <FormInput name="username" placeholder="user420437889" ref={usernameRef}></FormInput>
+        <FormInput name="name" placeholder="your name"></FormInput> 
+        <FormInput name="email" placeholder="example@mail.com"></FormInput>
+        <FormInput name="tel" placeholder="+00 000-000-000"></FormInput> 
+        <button>Submit</button>
       </form>
     </>
   )
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import { useRef } from "react";
 
 export default function FormInput(props) {
 	return (
 		<div className="py-2">
 			<label>Username</label>
 			<input
-				type="text"
+				ref={props.ref}
+				name={props.name}
+                type="text"
 				className="border-1 ml-2 p-1 rounded-sm"
 				placeholder={props.placeholder}
-				onChange={(e) => props.setUsername(e.target.value)}
 			/>
 		</div>
 	);
